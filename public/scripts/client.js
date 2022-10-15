2601f7226e7e4478bb36372184efff8ce8991d5d
Nick Dellar <ndelfern@live.ca>
2022-10-15 20:24:01 +0000

    Add AJAX post method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const data = [{}];
 
 $(document).ready(function() {
-  const createTweetElement = function (tweet) {
-  const $tweet = `
+  const createTweetElement = function(tweet) {
+    const $tweet = `
   <article class='tweets-container'>
   <header id="tweets">
     <pic class=${tweet.user.avatars}></pic>
@@ -48,19 +23,29 @@ $(document).ready(function() {
     <i class="fa-solid fa-retweet"></i>
     <i class="fa-solid fa-heart"></i>
   </footer>
-</article>`
-return $tweet;
-}
+</article>`;
+    return $tweet;
+  };
 
-const renderTweets = function(tweets) {
-  tweets.forEach(element => {
-    const tweet = createTweetElement(element)
-  $('#tweets-container').append(tweet);
+  const renderTweets = function(tweets) {
+    tweets.forEach(element => {
+      const tweet = createTweetElement(element);
+      $('#tweets-container').append(tweet);
+    });
+  };
+  renderTweets(data);
 });
-}
 
-renderTweets(data);
+$(function() {
+  //Post tweet without reloading page
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const text = $("form").serialize();
+    $.ajax({method:"POST", url:'/tweets', data: text})
+      .then((res) => console.log(res));
+  });
+})
 
-  // to add it to the page so we can make sure it's got all the right elements, classes, et
-});
+
+
 
